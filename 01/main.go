7b3c3e102405ef1b9312e935440f40c4af098bcc
José Jonathan Romero Martínez <vyr0n@live.com.mx>
2017-02-20 21:14:21 -0600

    I put information in the file README.md about this project and what it is about.

--- 01/main.go
+++ 01/main.go
@@ -9,6 +9,7 @@ func main() {
   var nombre, apellido string 
   nombre = "Jonathan"
   apellido = " Romero"
+  edad, direccion := 27, "Interoceanico" 
 
-	fmt.Println(nombre, apellido)
+	fmt.Println(nombre, apellido, edad, direccion )
 }
