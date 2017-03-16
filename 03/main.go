edbc3a1a49b3a936ff7c180cd10d456886f36abe
José Jonathan Romero Martínez <vyr0n@live.com.mx>
2017-03-16 12:00:05 -0600

    This is a file with conditional examples.

---
+++ 03/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+		if 132 < 323{
+			fmt.Println("Hi, this is higher.")
+		}
+
+		fmt.Println("Fin del programa.")
+
+		if 10 > 5 && 1 < 7{
+			fmt.Println("Hi, this and is true")
+		}
+		if 10 < 5 || 1 < 7{
+			fmt.Println("Hi, this or is true")
+		}
+		if !(10 < 5 ){
+			fmt.Println("Hi, this negative is true")
+		}
+		if isValid :=  true; isValid {
+			fmt.Println("Hi, this is Valid is true")
+		}
+		
+		
+		
+}
