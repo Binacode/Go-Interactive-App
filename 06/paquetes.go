bfa2163f3cc986f71cb73708554f6d2e0c126343
José Jonathan Romero Martínez <vyr0n@live.com.mx>
2017-07-18 16:24:07 -0500

    Solution for a one mistake.

--- 06/paquetes.go
+++ 06/paquetes.go
@@ -1,15 +1,16 @@
 package main
 
-import 	"C:\\Users\\JJ\\Documents\\Websites\\Go-Interactive-App\\06\\saludar.go"
+import 	(
+	"fmt"
+
+	"C:\\Users\\JJ\\Documents\\Websites\\Go-Interactive-App\\06\\saludar.go"
+)
 
 func main(){
-
 		// nombre :=
 		// "Jonathan"
 		// saludar.Saludar(nombre)
 		saludar.Saludar("Jonathan")
 		saludar.MeVes = "Esto es un texto asignado desde el main"
-		fmt.Println(saludar.Meves)
-
-		
+		fmt.Println(saludar.MeVes)
 }
